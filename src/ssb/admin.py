6317996f45accee493b0506d5dd1934445bed19f
Carlos Valiente <carlos.valiente@ecmwf.int>
2011-05-10 16:55:05 +0100

    Changes required by Cesar

--- src/ssb/admin.py
+++ src/ssb/admin.py
@@ -1,27 +1,56 @@
+import datetime
+
 from django.contrib import admin
+from django import forms
+from django.forms import widgets
 
 from ssb import models
 
 
+def next_wednesday_10h():
+    today = datetime.datetime.today()
+    weekday = today.weekday()
+    if weekday < 2:
+        next_wed = today + datetime.timedelta(days=2 - weekday)
+    else:
+        next_wed  =today + datetime.timedelta(days=9 - weekday)
+    return next_wed.replace(hour=10, minute=0, second=0)
+
+
+class SystemSessionForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        kwargs["initial"] = {"start_date": next_wednesday_10h()}
+        forms.ModelForm.__init__(self, *args, **kwargs)
+
+    class Meta:
+        model = models.SystemSession
+        widgets = {
+            "impact_probability": widgets.RadioSelect,
+            "impact_severity": widgets.RadioSelect,
+        }
+
+
 class SystemSessionAdmin(admin.ModelAdmin):
     
-    list_display = ("title", "start_date", "status")
-    list_filter = ("status",)
-    search_fields = ("title", "description")
-    date_hierarchy = "start_date"
     actions = ("approve_sessions", "cancel_sessions")
+    date_hierarchy = "start_date"
+    form = SystemSessionForm
+    list_display = ("title", "start_date", "approved")
+    list_filter = ("approved",)
+    search_fields = ("title", "purpose")
 
     def approve_sessions(self, request, queryset):
-        queryset.update(status="approved")
+        queryset.update(approved=True)
     approve_sessions.short_description = "Approve system sessions"
 
     def cancel_sessions(self, request, queryset):
-        queryset.update(status="canceled")
+        queryset.update(approved=False)
     cancel_sessions.short_description = "Cancel system sessions"
 
     def get_actions(self, request):
         actions = super(SystemSessionAdmin, self).get_actions(request)
-        if not request.user.has_perm("ssb.can_change_status"):
+        if not request.user.has_perm("ssb.can_approve"):
             del actions["approve_sessions"]
             del actions["cancel_sessions"]
         return actions
--- src/ssb/models.py
+++ src/ssb/models.py
@@ -8,26 +8,73 @@ __all__ = [
 
 class SystemSession(models.Model):
 
-    STATUS_CHOICES = (
-        ("pending", "Aproval pending"),
-        ("approved", "Approved"),
-        ("rejected", "Rejected"),
-        ("canceled", "Canceled"),
+    URGENCY_CHOICES = (
+        ("default", "Default"),
+        ("high", "High")
     )
 
+    IMPACT_CHOICES = (
+        ("definite", "Definite"),
+        ("potential", "Potential")
+    )
+
+    IMPACT_PROBABILITY_CHOICES = ((i, i) for i in range(6))
+
+    IMPACT_SEVERITY_CHOICES = ((i, i) for i in range(6))
+
     title = models.CharField(max_length=256)
-    description = models.TextField()
+    services = models.CharField(max_length=128,
+                                help_text="Systems and services that will be "
+                                          "worked on")
     start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    status = models.CharField(choices=STATUS_CHOICES,
-                              max_length=max(len(c[0]) for c in STATUS_CHOICES),
-                              default="pending",
-                              editable=False)
+    impl_time = models.PositiveIntegerField(verbose_name="Implementation time",
+                                            help_text="Length (in minutes) "
+                                                      "of the system session")
+    rollback_time = models.PositiveIntegerField(help_text="Time (in minutes) "
+                                                          "required for "
+                                                          "rollback")
+    responsible = models.CharField(max_length=128,
+                                   help_text="Person(s) involved")
+    purpose = models.TextField()
+
+    urgency = models.CharField(choices=URGENCY_CHOICES,
+                               max_length=32,
+                               default="default")
+    impact_level = models.CharField(choices=IMPACT_CHOICES,
+                                    max_length=32,
+                                    blank=True)
+    ops_impact = models.TextField(verbose_name="Operational impact",
+                                  help_text="Expected impact on operations")
+    high_risk_period = models.BooleanField(default=False,
+                                           help_text="High risk period "
+                                                     "requested")
+
+    svc_impact = models.TextField(verbose_name="Impact on services",
+                                  help_text="Expected impact on other "
+                                            "services")
+    impact_probability = models.PositiveIntegerField(default=0,
+                                                     choices=IMPACT_PROBABILITY_CHOICES,
+                                                     help_text="0: Unlikely, "
+                                                               "5: Almost certain")
+    impact_severity = models.PositiveIntegerField(default=0,
+                                                  choices=IMPACT_SEVERITY_CHOICES,
+                                                  help_text="0: No impact, "
+                                                            "5: Very serious")
+    worst_case_scenario = models.TextField(help_text="Worst case scenario "
+                                                     "and exit strategy")
+
+    standby = models.CharField(max_length=256, blank=True,
+                               help_text="Person(s) required on standby "
+                                         "during the session")
+    approved = models.BooleanField(default=False, editable=False)
+
+    owner = models.CharField(max_length=128,
+                             editable=False)
 
     class Meta:
         ordering = ["start_date"]
         permissions = (
-            ("can_change_status", "Can approve or cancel system sessions"),
+            ("can_approve", "Can approve system sessions"),
         )
 
     def __unicode__(self):
